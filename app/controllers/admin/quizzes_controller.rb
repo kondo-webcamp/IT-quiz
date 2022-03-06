274b0fb31f5bc870ee87556d2c0ea7e98df270b3
kondo-webcamp <ck0729nh@icloud.com>
2022-03-06 04:43:18 +0000

    クイズ作成機能

--- app/controllers/admin/quizzes_controller.rb
+++ app/controllers/admin/quizzes_controller.rb
@@ -1,2 +1,20 @@
 class Admin::QuizzesController < ApplicationController
+  def new
+    @quiz = Quiz.new
+  end
+
+  def index
+    @quizzes = Quiz.all
+  end
+
+  def create
+    @quiz = Quiz.new(quiz_params)
+    @quiz.save
+    redirect_to admin_quizzes_path
+  end
+
+  private
+  def quiz_params
+    params.require(:quiz).permit(:question, :answer, :choices_first, :choices_second, :choices_third, :choices_fourth, :explanation)
+  end
 end
