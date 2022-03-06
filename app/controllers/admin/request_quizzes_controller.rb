4b0e72a4f61ae050ad551d2987379422bf4e4fdc
kondo-webcamp <ck0729nh@icloud.com>
2022-03-06 16:15:19 +0000

    ユーザーのクイズ作成を管理者側で表示

--- app/controllers/admin/request_quizzes_controller.rb
+++ app/controllers/admin/request_quizzes_controller.rb
@@ -1,2 +1,9 @@
 class Admin::RequestQuizzesController < ApplicationController
+  def index
+    @request_quizzes = RequestQuiz.all
+  end
+
+  def show
+    @request_quiz = RequestQuiz.find(params[:id])
+  end
 end
--- app/controllers/user/request_quizzes_controller.rb
+++ app/controllers/user/request_quizzes_controller.rb
@@ -1,2 +1,16 @@
 class User::RequestQuizzesController < ApplicationController
+  def new
+    @request_quiz = RequestQuiz.new
+  end
+
+  def create
+    @request_quiz = RequestQuiz.new(request_quiz_params)
+    @request_quiz.save
+    redirect_to user_request_quizzes_path
+  end
+
+  private
+  def request_quiz_params
+    params.require(:request_quiz).permit(:question, :answer, :choices_first, :choices_second, :choices_third, :choices_fourth, :explanation)
+  end
 end
