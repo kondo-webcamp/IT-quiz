ef9970e5e298a42a024b76f8ab10098787b7aa02
kondo-webcamp <ck0729nh@icloud.com>
2022-03-06 04:58:37 +0000

    クイズ内容変更機能

--- app/controllers/admin/quizzes_controller.rb
+++ app/controllers/admin/quizzes_controller.rb
@@ -13,6 +13,17 @@ class Admin::QuizzesController < ApplicationController
     redirect_to admin_quizzes_path
   end
 
+  def edit
+    @quiz = Quiz.find(params[:id])
+  end
+
+  def update
+    @quiz = Quiz.find(params[:id])
+    @quiz.update(quiz_params)
+    flash[:notice] = "変更しました"
+    redirect_to admin_quizzes_path
+  end
+
   private
   def quiz_params
     params.require(:quiz).permit(:question, :answer, :choices_first, :choices_second, :choices_third, :choices_fourth, :explanation)
