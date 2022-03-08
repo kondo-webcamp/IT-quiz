9807fff7b35a864efe95b861ee08d0e8df23d33c
kondo-webcamp <ck0729nh@icloud.com>
2022-03-08 10:40:55 +0000

    クイズを重複せずランダム出題

--- app/controllers/user/quizzes_controller.rb
+++ app/controllers/user/quizzes_controller.rb
@@ -4,28 +4,22 @@ class User::QuizzesController < ApplicationController
      arr = Quiz.all.pluck(:id) # arr = ["1","2","3"...]
 
     if params[:count]
-      # 1問~10問
-
-      new_arr = arr - params[:used_quizes].split("").map(&:to_i)
+      # 2問~10問
+      new_arr = arr - params[:used_quizes].split(",").map(&:to_i)
       # arr - params[:used_quizes]　回答した問題をarr配列から引いていく
-      # split("")　答えた問題のidを文字列にする [1]=>["1"]
-      # map(&:to_i)  arrの中身全てをintegerに変換　["1","2","3"]=>[1,2,3]
 
       @quiz = Quiz.find(new_arr.shuffle[0])
       # すでに答えた問題を抜いた配列の中からランダムで出題
 
-      @count =  params[:count].to_i + 1  # "1"=> 1
+      @count = params[:count].to_i + 1
       @used_quizes = params[:used_quizes].split(",").push(@quiz.id).join(",")
-      # split(",") 問題、答え、選択肢などを1つの文字列として変換
+      # split(",") 配列にし、出題した問題を配列にいれ、表示するためにjoinを使用
 
     else
-      #終了
-
+      #1問目
       @count = 1
-      #1問目に戻る 1に戻しておく
-
-      @used_quizes = ""
       @quiz = Quiz.find(arr.shuffle[0])
+      @used_quizes = @quiz.id
     end
 
     def new
