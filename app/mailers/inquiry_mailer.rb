a16776ccc34140d8e1e8522264dd767e7d295244
kondo-webcamp <ck0729nh@icloud.com>
2022-03-13 06:43:58 +0000

    ファイル修正

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,10 +1,2 @@
 class InquiryMailer < ApplicationMailer
-  def send_mail(inquiry)
-    @inquiry = inquiry
-    mail(
-      from: 'system@example.com',
-      to:   'c.kondo.pr@gmail.com',
-      subject: 'お問い合わせ通知'
-    )
-  end
 end
